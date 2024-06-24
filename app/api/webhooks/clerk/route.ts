27c6b134d189f60afbb7976999a5d38f78e90f73
Sanele Hlongwane <sanelehlongwane61@gmail.com>
2024-06-24 12:55:58 +0200

    third  commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,4 @@
-//app/webhooks/clerk
-
+// app/webhooks/clerk.ts
 import { Webhook } from "svix";
 import { WebhookEvent, clerkClient } from "@clerk/clerk-sdk-node";
 import { headers } from "next/headers";
@@ -13,38 +12,27 @@ export async function POST(req: Request) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
 
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occurred -- no svix headers', {
-      status: 400
-    });
+    return new Response('Error occurred -- no svix headers', { status: 400 });
   }
 
-  // Get the body
-  let payload;
+  let payload: any;
   try {
     payload = await req.json();
   } catch (err) {
     console.error('Error parsing JSON:', err);
-    return new Response('Invalid JSON', {
-      status: 400
-    });
+    return new Response('Invalid JSON', { status: 400 });
   }
 
   const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
-
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -53,13 +41,9 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occurred', {
-      status: 400
-    });
+    return new Response('Error occurred', { status: 400 });
   }
 
-  // Handle the event
-  const { id } = evt.data;
   const eventType = evt.type;
 
   if (eventType === "user.created") {
@@ -89,7 +73,7 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "New user created", user: newUser });
   }
 
-  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
+  console.log(`Webhook with an ID of ${evt.data.id} and type of ${eventType}`);
   console.log('Webhook body:', body);
 
   return new Response('', { status: 200 });
